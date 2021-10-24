660eb88053c1b51132778df17fef50f07a20a307
Victor Montivero <victorabel46@gmail.com>
2021-10-24 20:29:07 -0300

    commit inicial sin crear entidades

---
+++ src/test/java/com/alkemy/iconocs/iconos/IconosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.iconocs.iconos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
